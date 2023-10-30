b39da5a88c72b018f92848ee56d56fa1178f8c33
caiquelms <caique.lemosferreira7@gmail.com>
2023-10-30 19:00:08 -0300

    att aula02, add aula03, exercicio pagina de login

--- aula/aula02/src/index.js
+++ aula/aula02/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Contador from "./pages/Contador";
+import Gatinhos from "./pages/Gatinhos";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ aula/pagina-login/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const entrar = () => {
+    const infos = {
+      usuario: usuario,
+      senha: senha,
+    };
+    console.log(infos);
+    setUsuario("");
+    setSenha("");
+
+    localStorage.setItem("info", JSON.stringify(infos));
+  };
+
+  return (
+    <>
+      <h1>LOGIN</h1>
+      <div className="usuario">
+        <p>Usuário</p>
+        <input
+          type="text"
+          placeholder="Digite seu usuário"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+        />
+      </div>
+      <div className="senha">
+        <p>Senha</p>
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+      </div>
+      <div className="card">
+        <button onClick={entrar}>Entrar</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
