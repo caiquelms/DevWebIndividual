8dba9c7c2889a938e459d2a039685e0e6409b02d
caiquelms <caique.lemosferreira7@gmail.com>
2023-10-31 22:17:27 -0300

    aula05

---
+++ aula/aula05/src/pages/TaskList/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const TaskList = () => {
+  const [novaTarefa, setNovaTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+  const cadastrar = () => {
+    setTarefas([...tarefas, novaTarefa]);
+    console.log([...tarefas, novaTarefa]);
+    setNovaTarefa("");
+  };
+
+  const excluirTarefa = (ind) => {
+    console.log("excluir tarefa" + ind);
+    const novoArray = tarefas.filter((item, index) => index != ind);
+    console.log(novoArray);
+    setTarefas(novoArray);
+  };
+
+  return (
+    <main>
+      <h1>Lista de Tarefas</h1>
+      <section>
+        <h2>Cadastre sua tarefa</h2>
+        <input
+          type="text"
+          placeholder="Cadastre uma tarefa"
+          value={novaTarefa}
+          onChange={(e) => setNovaTarefa(e.target.value)}
+        />
+        <button onClick={cadastrar}>+</button>
+      </section>
+      <section>
+        <h2>Lista de Tarefas</h2>
+        {tarefas.map((item, index) => (
+          <div key={index}>
+            <h3>{item}</h3>
+            <button onClick={() => excluirTarefa(index)}>Excluir</button>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default TaskList;
